43998e4311a664d5da932cbd115350549346273c
Mohadese Amari <mohadese.amari@c1tech.group>
2026-07-15 23:31:09 +0330

    fix: redirect default route to login

--- namabar-panel/src/routes/AppRouter.jsx
+++ namabar-panel/src/routes/AppRouter.jsx
@@ -6,20 +6,20 @@ import Inbox from '../pages/Inbox';
 import Outbox from '../pages/Outbox';
 import SendFax from '../pages/SendFax';
 
-
 export default function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
 
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/faxes/inbox" element={<Inbox />} />
-        <Route path="/faxes/sent" element={<Outbox />} />   
+        <Route path="/faxes/sent" element={<Outbox />} />
         <Route path="/faxes/send" element={<SendFax />} />
       </Route>
 
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
